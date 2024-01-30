4a01c5c75a6a681b2e7ed9d9af672475e0e52af1
LuisChavez04 <lc002539@gmail.com>
2024-01-30 13:23:57 -0800

    Update Hello.cpp

--- Lab1/Hello.cpp
+++ Lab1/Hello.cpp
@@ -9,5 +9,7 @@ using namespace std;
 int main() {
     cout << "Hello World!" << endl;
     printf("Hello World again!\n");
+    printf("This is nothing like Python!\n");
+    cout << "At first this was really challenging to comprehend" << endl;
     return 0;
 }
