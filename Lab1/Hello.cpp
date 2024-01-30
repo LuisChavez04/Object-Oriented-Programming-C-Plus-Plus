d141d7b2a742f06f6c89179813314fd2fa5a3ef0
LuisChavez04 <lc002539@gmail.com>
2024-01-29 19:55:11 -0800

    Created Hello.cpp

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/***
+ * {Short Description of the Lab}
+ ***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
