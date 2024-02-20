cbe70bff69c7a8e1eb915afd545cf65690a09b49
LuisChavez04 <lc002539@gmail.com>
2024-02-20 13:13:51 -0800

    Updated Lab4
    Added the functions sumOfThree and division. These functions return their mathematical solutions.

--- Lab4/Lab4_func.cpp
+++ Lab4/Lab4_func.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 using namespace std;
 
+/** sumOfThree
+* @brief returns the sum of x, y, and z
+*
+* @param x : (int)
+* @param y : (int)
+* @param z : (int)
+* @return int
+*/
 int sumOfThree(int x, int y, int z){
-    return x + y + z
-    /** sumOfThree
-    * @brief returns the sum of x, y, and z
-    *
-    * @param x : (int)
-    * @param y : (int)
-    * @param z : (int)
-    * @return int
-    */
+    return x + y + z;
+}
+
+/** division
+* @brief returns x / y
+*
+* @param x : (double)
+* @param y : (double)
+* @return double
+*/
+double division(double x, double y){
+    return x / y;
 }
