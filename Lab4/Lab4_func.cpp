90a952a496a5f2cd32121493ad3fd6cb49897af7
LuisChavez04 <lc002539@gmail.com>
2024-02-20 13:10:02 -0800

    Starting the build of the code

---
+++ Lab4/Lab4_func.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int sumOfThree(int x, int y, int z){
+    return x + y + z
+    /** sumOfThree
+    * @brief returns the sum of x, y, and z
+    *
+    * @param x : (int)
+    * @param y : (int)
+    * @param z : (int)
+    * @return int
+    */
+}
