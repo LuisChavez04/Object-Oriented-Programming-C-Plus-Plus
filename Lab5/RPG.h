9f3501897fb366bb1c8ed0e70db1613e54f87ed2
LuisChavez04 <lc002539@gmail.com>
2024-02-27 13:19:43 -0800

    Updated RPG.h

--- Lab5/RPG.h
+++ Lab5/RPG.h
@@ -1,5 +1,7 @@
 #define RPG_H
 
+#include <iostream>
+#include <string>
 using namespace std;
 
 const int SKILL_SIZE = 2;
@@ -10,7 +12,14 @@ class RPG {
         RPG();
         RPG(string name, int health, int strength, int defense, string type);
 
-        //Complete the rest
+        RPG::RPG();
+        RPG::RPG(string, int, int, int, string);
+        
+        void setSkills();
+        void printAction(string, RPG);
+        void updateHealth(int);
+        void attack(RPG *);
+        void useSkill(RPG *);
 
     private:
         //Complete the rest
