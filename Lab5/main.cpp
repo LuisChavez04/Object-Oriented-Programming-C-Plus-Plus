6922e67f92c610f7dd5c5452949e1ba57d4a9dba
LuisChavez04 <lc002539@gmail.com>
2024-02-29 13:45:41 -0800

    Added code to implement RPG.h and RPG.cpp

---
+++ Lab5/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include "RPG.h"
+
+using namespace std;
+
+int main()
+{
+    RPG p1 = RPG("Wiz", 70, 45, 15, "mage");
+    RPG p2 = RPG();
+
+    printf("%s Current Stats\n", p1.getName().c_str());
+    printf("Health: %i\t Strength: %i\t Defense: %i\n", p1.getHealth(), p1.getStrength(), p1.getDefense());
+
+    printf("%s Current Stats\n", p2.getName().c_str());
+    printf("Health: %i\t Strength: %i\t Defense: %i\n", p2.getHealth(), p2.getStrength(), p2.getDefense());
+
+
+
+
+
+
+}
