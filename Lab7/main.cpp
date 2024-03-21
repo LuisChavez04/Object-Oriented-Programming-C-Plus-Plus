4d37a269b74a06370d483b51560bb3b8b458e49b
LuisChavez04 <lc002539@gmail.com>
2024-03-21 13:39:49 -0700

    Updated the code
    Still need to implement the readFile function correctly

--- Lab7/main.cpp
+++ Lab7/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 using namespace std;
 
 /**
@@ -8,23 +9,40 @@ using namespace std;
  * 1
  * 2
  * 3
+ *
+ * 
  * 
  * @param filename 
  * @param arr 
- * @param sample_size 
+ * @param sample_size
+ * 
+ * array_int : 1 2 3 4 5 6 7 8 9 10
+ * index (i) : 0 1 2 3 4 5 6 7 8 9
+ * index addressable:
+ * cout << array_int[0]; // 1
+ * cout << array_int[9]; // 10 
  */
 void writeToFile(string filename, int * arr, int sample_size){
     ofstream file(filename);
     int val;
 
-    for(int i = 0; i < sample_size; i++){
+    for(int i = 0; i < sample_size; i++){ //loop 1 - 9
         file << arr[i];
         file << "\n";
+        // i        : 2
+        // arr[i]   : 2
+        /*
+        file contents:
+            1\n
+            2\n
+            3\n
+        */
     }
     file.close();
     printf("Wrote to %s\n", filename.c_str());
 }
 
+//---------------------------------------------------------------
 /**
  * @brief writes sample_size number of strings
  * Example output: if n = 2 and arr = ["cat", "dog"]
@@ -43,9 +61,10 @@ void writeToFile(string filename, string * arr, int sample_size){
         file << "\n";
     }
     file.close();
-    printf("%s\n", filename.c_str());
+    printf("wrote to %s\n", filename.c_str());
 }
 
+//-------------------------------------------------------------
 /**
  * @brief writes sample_size number of char
  * Example outpute: if n = 2 and arr = ["c", "d"]
@@ -65,9 +84,10 @@ void writeToFile(string filename, char * arr, int sample_size){
         file << "\n";
     }
     file.close();
-    printf("%s\n", filename.c_str());
+    printf("Wrote to %s\n", filename.c_str());
 }
 
+//---------------------------------------------------------------
 /**
  * @brief reads the content of any file
  * 
@@ -75,6 +95,12 @@ void writeToFile(string filename, char * arr, int sample_size){
  */
 void readFile(string filename){
     ifstream file(filename);
+    if(file.fail()){
+        cout << "Couldn't open file " << filename << endl;
+    
+        file.close();
+        return;
+    }
 }
 
 
@@ -82,7 +108,7 @@ void readFile(string filename){
 int main(){
     const int SAMPLE_SIZE = 10;
     int array_int[SAMPLE_SIZE] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    string array_int[SAMPLE_SIZE] = {"This", "is", "a", "sentence.",
+    string array_str[SAMPLE_SIZE] = {"This", "is", "a", "sentence.",
                                     "This", "is", "another", "sentence.",
                                     "The", "end."};
 
@@ -95,13 +121,21 @@ int main(){
 
     //Create a string variable as the file name and assign
     //it the value "array_str.csv"
-
     //CALL writeToFile using the string variable above, array_str, and SAMPLESIZE as
     //parameters
+
+    string str_file = "array_str.csv";
+    writeToFile(str_file, array_str, SAMPLE_SIZE);
+
+    string char_file = "array_char.csv";
+    writeToFile(char_file, array_char, SAMPLE_SIZE);
+
 
 
 
     //CALL readFile() on all three generated files
+    string filename = "display_functions";
+    readFile(filename);
 
 
     return 0;
