438c5d5aabdc57942a3d3696807ac7eda6db1882
LuisChavez04 <lc002539@gmail.com>
2024-03-26 13:03:43 -0700

    Create vector.cpp

---
+++ Lab8/vector.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void printMemArr(const int * arr, int size) {
+    printf
+}
+
+void printMemVec(vector<int> v){
+    printf("Vector - Each int is worth %lu bytes\n", sizeof(v[0]));
+}
+
+void incArrBy10(vector<int> & v){
+    for(int i = 0; )
+}
